8ba278bbb72a87ea678d134b259ca7038a305508
Anant Tyagi <ananttyagi020@gmail.com>
2026-03-09 21:08:52 +0530

    Added the solver (main) function, and the first pass function

--- co_2026_evaluation_framework_release/co_2026_evaluation_framework_release/SimpleAssembler/Assembler.py
+++ co_2026_evaluation_framework_release/co_2026_evaluation_framework_release/SimpleAssembler/Assembler.py
@@ -41,5 +41,108 @@ U_type={
 "auipc":"0010111"}
 J_type={
 "jal":"1101111"}
-labels = {}
+instruction_type={"add":"R","sub":"R","sll":"R","slt":"R","sltu":"R","xor":"R","srl":"R","or":"R","and":"R","addi":"I","sltiu":"I","lw":"I","jalr":"I","sw":"S","beq":"B","bne":"B","blt":"B","bge":"B","bltu":"B","bgeu":"B","lui":"U","auipc":"U","jal":"J"}
+encoder={
+"R":encode_R,
+"I":encode_I,
+"S":encode_S,
+"B":encode_B,
+"U":encode_U,
+"J":encode_J
+}
+labels ={}
 pc=0
+
+def n_bit_binary_converter(n, num):
+    global pc
+    if num not in range(-1*2**(n-1),2**(n-1)):
+        errorHandling("Syntax Error: Immediate out of range")
+    
+    if num>=0:
+        binary_with_extra=bin(num)
+        binary_list=binary_with_extra.split("b")
+        binary_string=binary_list[1]
+    else:
+        temp_num=2**n+num
+        binary_with_extra=bin(temp_num)
+        binary_list=binary_with_extra.split("b")
+        binary_string=binary_list[1]
+    
+    if len(binary_string)<n:
+        temp_string="0"*(n-len(binary_string))
+        binary=temp_string+binary_string
+    else:
+        binary=binary_string
+    
+    return binary
+
+def first_pass(lines):
+    global labels, pc, instructions
+    for line in lines:
+        if(line.strip() == ""): continue
+        if(":" in line):
+            label, extra = line.split(":", 1)
+            if(label.strip() in labels):
+                errorHandling(f"Duplicate label {label} found.")
+
+            labels[label.strip()] = pc
+
+            #to manage lines of type:
+            # label:
+            # stuff on the next line
+            if(extra.strip() != ""):
+                instructions.append(extra.strip())
+                pc += 4
+
+        else:
+            instructions.append(line.strip())
+            pc += 4
+            
+def solver():
+    global instructions, pc, encoder
+    in_path = sys.argv[1]
+    out_path = sys.argv[2]
+
+    read_path = None
+    if len(sys.argv) > 3:
+        read_path = sys.argv[3]
+
+    with open(in_path, "r") as assembly_code:
+        lines = assembly_code.readlines()
+
+    #lines is yet to be formatted
+
+    readable = None
+    if read_path:
+        readable = open(read_path, "w")
+
+    first_pass(lines)
+    pc = 0
+
+    flag = False
+    with open(out_path, "w") as machine_code:
+        for trav in instructions:
+            if(isHalt(trav)):
+                flag = True
+                if(trav != instructions[-1]):
+                    print(f"Invalid instruction occured at {pc//4 + 1}. Virtual Halt should be the last instruction")
+            
+            ins = trav.split()[0]
+
+            if (ins not in instruction_type):
+                errorHandling(f"The instruction {ins} does not exist")
+
+            #Also check the instruction for Virtual Halt.
+
+            ins_type = instruction_type[ins]
+            # print(len(encoder[ins_type](trav)))
+            machine_code.write(encoder[ins_type](trav) + '\n')
+            pc += 4
+
+        if (flag == False):
+            print("Virtual Halt is missing")
+            
+    if(readable):
+        readable.close()
+        
+solver()
